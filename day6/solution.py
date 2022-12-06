7f27cf49fa6ac1b546b3b6599245044069b970e4
Eric Tamme <eric@uphreak.com>
2022-12-06 09:41:26 -0500

    day6 solution

--- day6/solution.py
+++ day6/solution.py
@@ -1,9 +1,6 @@
-import sys
-for line in sys.stdin:
-  line = line.strip()
-  # for part 2, change to size=14
-  size = 4
-  for index in range(len(line)):
-    if(len(line[index:index+size]) == len(set(line[index:index+size]))):
-      print("marker: "+str(index+size))
-      break
+line = open("/home/etamme/sandbox/aoc-2022/day6/input.txt", "r").read().strip()
+size = 4 # for part 2, change to size=14
+for index in range(len(line)):
+  if(len(line[index:index+size]) == len(set(line[index:index+size]))):
+    print("marker: "+str(index+size))
+    break
